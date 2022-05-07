d894cb0624acfc734054f8b57dd3cdf52d2cf2cd
mk3250d07295y@aol.com <Yoika>
2022-05-07 20:44:46 +0900

    update options style

--- src/options/options.js
+++ src/options/options.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './options.scss'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <h1>Hello Options</h1>
+  <section className="options">
+    <h1>Hello Options</h1>
+    <p className="options__description">This is Google Extension Option Sample</p>
+  </section>
 );
