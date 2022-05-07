aef207da9b28b50e0bf3f69c4db72efa9a4c1195
mk3250d07295y@aol.com <Yoika>
2022-05-07 20:41:42 +0900

    update popup style

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './popup.scss'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <h1>Hello Popup</h1>
+  <sesction className="popup">
+    <h1>Google Chrome Extension Popup</h1>
+    <p className="popup__description">This is Chrome Extension Popup Sample</p>
+  </sesction>
 );
