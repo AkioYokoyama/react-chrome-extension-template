40394aad411d6197650acaec3b1431979b3f43ea
mk3250d07295y@aol.com <Yoika>
2022-07-04 21:59:59 +0900

    update 関数コンポーネント化

--- src/options/Options.tsx
+++ src/options/Options.tsx
@@ -1,14 +1,19 @@
 import * as React from 'react';
+import { FC } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import './options.scss'
 
-const rootEl = document.getElementById('root')
-if (rootEl) {
-  const root = ReactDOM.createRoot(rootEl);
-  root.render(
+const Options: FC = () => {
+  return (
     <section className="options">
       <h1>Hello Options</h1>
       <p className="options__description">This is Google Extension Option Sample</p>
     </section>
   );
 }
+
+const rootElement = document.getElementById('root');
+// https://blog.logrocket.com/how-to-use-typescript-with-react-18-alpha/
+if (!rootElement) throw new Error('Failed to find the root element');
+const root = ReactDOM.createRoot(rootElement);
+root.render(<Options />);
